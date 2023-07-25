444126623820a1fc5a4ec2536d0c1bef9a0035ca
Helbergm <130799722+Helbergm@users.noreply.github.com>
2023-07-24 22:36:01 -0500

    first commit

---
+++ src/Final.js
@@ -0,0 +1,12 @@
+import React from "react";
+import imagen from './imagenes/vulture.png';
+
+function Selectores(){
+    return (
+        <div>
+            <img src={imagen}/>
+        </div>
+    );
+}
+export default Selectores;
+
